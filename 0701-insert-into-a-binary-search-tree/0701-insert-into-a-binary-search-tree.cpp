e9d2b497ec61ecca8b95e657193e9dba2772f11b
Md Farhan Ahmad <152099785+mdfarhanahmad777@users.noreply.github.com>
2026-07-17 15:51:42 +0530

    Time: 0 ms (100%), Space: 59.5 MB (25.17%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
@@ -16,10 +16,29 @@ public:
         if (root == NULL)
             return new TreeNode(val);
 
-        if (val < root->val)
-            root->left = insertIntoBST(root->left, val);
-        else
-            root->right = insertIntoBST(root->right, val);
+        TreeNode* curr = root;
+
+        while (true) {
+
+            if (val < curr->val) {
+
+                if (curr->left == NULL) {
+                    curr->left = new TreeNode(val);
+                    break;
+                }
+
+                curr = curr->left;
+            }
+            else {
+
+                if (curr->right == NULL) {
+                    curr->right = new TreeNode(val);
+                    break;
+                }
+
+                curr = curr->right;
+            }
+        }
 
         return root;
     }
